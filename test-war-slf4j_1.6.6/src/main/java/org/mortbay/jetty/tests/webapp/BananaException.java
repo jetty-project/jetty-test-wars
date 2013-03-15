19979b7751ad7e2aa067b453fff2a47c569c3f6d
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-03-15 09:31:59 -0700

    Adding test wars for slf4j 1.6.6 and 1.7.2
     + Adding application specific throwables as well, to test for
       possible classloader constraint violation issues.

---
+++ test-war-slf4j_1.6.6/src/main/java/org/mortbay/jetty/tests/webapp/BananaException.java
@@ -0,0 +1,34 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2013 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.mortbay.jetty.tests.webapp;
+
+/**
+ * WebApp specific Exception.
+ * <p>
+ * Here to make sure that the centralized logging doesn't have
+ * a classpath / classloader choke on this kind of log.
+ */
+@SuppressWarnings("serial")
+public class BananaException extends RuntimeException
+{
+    public BananaException(String msg)
+    {
+        super(msg);
+    }
+}
